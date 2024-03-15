b2af27255ce3c5d9396dba5285afae10c82abcba
Sharanya Dabas <sd699@cornell.edu>
2024-03-14 20:19:54 -0400

    Created dune file

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,3 @@
 (** Print the interface with the following format: Position on board: <users>
     Money for each player: <users> Properties owned: <users>*)
-let print_info () = failwith "todo"
+let print_info () = "todo"
