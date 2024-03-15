624663781c48278a516dc3d3205588f20ea42874
Sharanya Dabas <sd699@cornell.edu>
2024-03-14 20:46:37 -0400

    Allow users to roll dice in main

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,22 @@
 (** Print the interface with the following format: Position on board: <users>
     Money for each player: <users> Properties owned: <users>*)
-let print_info () = "todo"
+let print_info () = print_endline "todo"
+
+(** Creates random number between 1-6*)
+let roll_dice () =
+  let () = Random.self_init () in
+  1 + Random.int 6
+
+(** Moves player n spots dependent on what the player rolled*)
+let move_player (n : int) = Printf.printf "player moved %d spots" n
+
+(** Begins game by asking player to type start*)
+let () =
+  let () = print_string "Type \"start\" to begin the game: " in
+  let the_input = read_line () in
+  if the_input = "start" then begin
+    print_info ();
+    let roll = roll_dice () in
+    move_player roll
+  end
+  else ()
