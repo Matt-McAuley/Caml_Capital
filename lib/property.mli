353b877bc755defcdd9df1825ee7cee6198c2a14
Erdis Brahimi <eb697@cornell.edu>
2024-03-23 21:37:59 -0400

    Implement color for properties

--- lib/property.mli
+++ lib/property.mli
@@ -1,22 +1,24 @@
 type t
 
 val get_name : t -> string
-(** [get_name] is returns the name of the property. *)
+(** [get_name] returns the name of the property. *)
 
 val get_pos : t -> int
-(** [get_pos] is returns the position of the property on the board. *)
+(** [get_pos] returns the position of the property on the board. *)
 
 val get_cost : t -> int
-(** [get_cost] is returns the cost to purchase the property. *)
+(** [get_cost] returns the cost to purchase the property. *)
 
 val get_rent : t -> int
-(** [get_rent] is returns the rent that a player landing on the property must
-    pay. *)
+(** [get_rent] returns the rent that a player landing on the property must pay. *)
+
+val get_color : t -> string
+(** [get_color] returns the color group that the property falls under. *)
 
 val ( = ) : t -> t -> bool
 (** [p1 = p2] returns true if property 1 and property 2 are the same property.
     Two properties are the same property if they have the same name. *)
 
 val create_property : string -> int -> int -> int -> t
-(** [create_property name pos cost rent] creates a property with the name
-    [name], position [pos], cost [cost], and rent [rent]. *)
+(** [create_property name pos cost rent color] creates a property with the name
+    [name], position [pos], cost [cost], rent [rent], and color [color]. *)
