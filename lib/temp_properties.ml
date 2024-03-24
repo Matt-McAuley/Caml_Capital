e2af26d338c5bb6e8b6909fd05f3589a38d939d1
Erdis Brahimi <eb697@cornell.edu>
2024-03-23 21:50:23 -0400

    Add color value to each property

--- lib/temp_properties.ml
+++ lib/temp_properties.ml
@@ -1,28 +1,28 @@
 open Property
 
-let go = create_property "GO!" 0 0 0
-let mediterraneanave = create_property "Mediterranean Avenue" 1 60 2
-let balticave = create_property "Baltic Avenue" 2 60 4
-let orientalave = create_property "Oriental Avenue" 3 100 6
-let vermontave = create_property "Vermont Avenue" 4 100 6
-let connecticutave = create_property "Connecticut Avenue" 5 120 8
-let stcharlesplace = create_property "St. Charles Place" 6 140 10
-let statesave = create_property "States Avenue" 7 140 10
-let virginiaave = create_property "Virginia Avenue" 8 160 12
-let stjamesplace = create_property "St. James Place" 9 180 14
-let tennesseeave = create_property "Tennessee Avenue" 10 180 14
-let newyorkave = create_property "New York Avenue" 11 200 16
-let kentuckyave = create_property "Kentucky Avenue" 12 220 18
-let indianaave = create_property "Indiana Avenue" 13 220 18
-let illinoisave = create_property "Illinois Avenue" 14 240 20
-let atlanticave = create_property "Atlantic Avenue" 15 260 22
-let ventnorave = create_property "Ventnor Avenue" 16 260 22
-let marvingardens = create_property "Marvin Gardens" 17 280 22
-let pacificave = create_property "Pacific Avenue" 18 300 26
-let northcarolinaave = create_property "North Carolina Avenue" 19 300 26
-let pennsylvaniaave = create_property "Pennsylvania Avenue" 20 320 28
-let parkplace = create_property "Park Place" 21 350 35
-let boardwalk = create_property "Boardwalk" 22 400 50
+let go = create_property "GO!" 0 0 0 "none"
+let mediterraneanave = create_property "Mediterranean Avenue" 1 60 2 "brown"
+let balticave = create_property "Baltic Avenue" 2 60 4 "brown"
+let orientalave = create_property "Oriental Avenue" 3 100 6 "light_blue"
+let vermontave = create_property "Vermont Avenue" 4 100 6 "light_blue"
+let connecticutave = create_property "Connecticut Avenue" 5 120 8 "light_blue"
+let stcharlesplace = create_property "St. Charles Place" 6 140 10 "pink"
+let statesave = create_property "States Avenue" 7 140 10 "pink"
+let virginiaave = create_property "Virginia Avenue" 8 160 12 "pink"
+let stjamesplace = create_property "St. James Place" 9 180 14 "orange"
+let tennesseeave = create_property "Tennessee Avenue" 10 180 14 "orange"
+let newyorkave = create_property "New York Avenue" 11 200 16 "orange"
+let kentuckyave = create_property "Kentucky Avenue" 12 220 18 "red"
+let indianaave = create_property "Indiana Avenue" 13 220 18 "red"
+let illinoisave = create_property "Illinois Avenue" 14 240 20 "red"
+let atlanticave = create_property "Atlantic Avenue" 15 260 22 "yellow"
+let ventnorave = create_property "Ventnor Avenue" 16 260 22 "yellow"
+let marvingardens = create_property "Marvin Gardens" 17 280 22 "yellow"
+let pacificave = create_property "Pacific Avenue" 18 300 26 "green"
+let northcarolinaave = create_property "North Carolina Avenue" 19 300 26 "green"
+let pennsylvaniaave = create_property "Pennsylvania Avenue" 20 320 28 "green"
+let parkplace = create_property "Park Place" 21 350 35 "dark_blue"
+let boardwalk = create_property "Boardwalk" 22 400 50 "dark_blue"
 
 let property_list =
   [
